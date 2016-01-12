90a8c61c917d6f48568280d8ed80d98abddecb78
Allen Goodman <allen.goodman@icloud.com>
2016-01-12 15:00:37 -0500

    travis

--- README.rst
+++ README.rst
@@ -1,2 +1,5 @@
 perturbation (WIP)
 ==================
+
+.. image:: https://travis-ci.org/0x00B1/persistence.svg
+    :target: https://travis-ci.org/0x00B1/persistence
--- setup.py
+++ setup.py
@@ -1,23 +1,54 @@
 import setuptools
+import setuptools.command.test
+import sys
+
+
+class Test(setuptools.command.test.test):
+    user_options = [
+        ("pytest-args=", "a", "Arguments to pass to py.test")
+    ]
+
+    def initialize_options(self):
+        setuptools.command.test.test.initialize_options(self)
+
+        self.pytest_args = []
+
+    def finalize_options(self):
+        setuptools.command.test.test.finalize_options(self)
+
+        self.test_args = []
+
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+
+        sys.exit(errno)
+
 
 setuptools.setup(
-    name='perturbation',
-    version='0.0.0',
-    author='Allen Goodman',
-    author_email='allen.goodman@icloud.com',
-    packages=setuptools.find_packages(
-            exclude=[
-                'test'
+        name='perturbation',
+        version='0.0.0',
+        author='Allen Goodman',
+        author_email='allen.goodman@icloud.com',
+        cmdclass={
+            'test': Test
+        },
+        packages=setuptools.find_packages(
+                exclude=[
+                    'test'
+                ]
+        ),
+        install_requires=[
+            'pandas',
+            'pytest',
+            'sqlalchemy'
+        ],
+        entry_points={
+            'console_scripts': [
+                'perturbation=perturbation:__main__',
             ]
-    ),
-    install_requires=[
-        'pandas',
-        'pytest',
-        'sqlalchemy'
-    ],
-    entry_points={
-        'console_scripts': [
-            'perturbation=perturbation:__main__',
-        ]
-    }
+        }
 )
