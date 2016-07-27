3d81e0d83d33c644adb569fa6621530501f4085a
Shantanu Singh <shsingh@broadinstitute.org>
2016-07-27 16:13:38 -0400

    Exclude default stage

--- perturbation/__init__.py
+++ perturbation/__init__.py
@@ -24,7 +24,7 @@ def __version__(context, parameter, argument):
 @click.help_option(help='')
 @click.option('-o', '--output', type=click.Path(exists=False, file_okay=True, writable=True))
 @click.option('-s', '--sqlfile', default='views.sql', type=click.Path(exists=True, file_okay=True, readable=True))
-@click.option('-t', '--stage', default='images', type=click.Choice(['images', 'objects']))
+@click.option('-t', '--stage', type=click.Choice(['images', 'objects']))
 @click.option('-v', '--verbose', default=False, is_flag=True)
 @click.option('-d', '--skipmunge', default=False, is_flag=True)
 @click.option('-V', '--version', callback=__version__, expose_value=False, is_eager=True, is_flag=True)
