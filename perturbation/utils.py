3314910cb2adc40914cea6f8c2ae9eb7745ad117
Shantanu Singh <shsingh@broadinstitute.org>
2016-08-05 18:29:45 -0400

    Added validate_csvs

--- perturbation/utils.py
+++ perturbation/utils.py
@@ -1,6 +1,7 @@
 import glob
 import os
 
+
 def find_directories(directory):
     directories = set()
 
@@ -12,3 +13,23 @@ def find_directories(directory):
         directories.add(pathname)
 
     return directories
+
+
+def validate_csvs(config, directory):
+    image_csv = os.path.join(directory, config["filenames"]["image"])
+
+    if not os.path.isfile(image_csv):
+        raise OSError("{} not found in {}. Skipping.".format(config["filenames"]["image"], directory))
+
+    pattern_csvs = [filename for filename in glob.glob(os.path.join(directory, '*.csv')) if filename not in [
+        os.path.join(directory, config['filenames']['image']),
+        os.path.join(directory, config['filenames']['object']),
+        os.path.join(directory, config['filenames']['experiment'])
+    ]]
+
+    filesize_checks = dict({(filename, os.stat(filename).st_size > 0) for filename in [*pattern_csvs, image_csv]})
+
+    if not all(filesize_checks.values()):
+        raise OSError("Some files were empty. Skipping {}.".format(directory))
+
+    return pattern_csvs, image_csv
