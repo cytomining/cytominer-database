0a088c5ed1363af63936c4b30148772356c7343d
Shantanu Singh <shsingh@broadinstitute.org>
2016-08-07 09:40:11 -0400

    Test for TableNumber uniqueness

--- test/test_ingest.py
+++ test/test_ingest.py
@@ -44,3 +44,6 @@ def test_seed(dataset):
             assert df.shape[0] == dataset["ingest"]["{}_nrows".format(table_name)]
 
             assert df.shape[1] == dataset["ingest"]["{}_ncols".format(table_name)] + 1
+
+            if table_key != "image":
+                df.groupby(["TableNumber", "ImageNumber"]).size().sum() == dataset["ingest"]["{}_nrows".format(table_name)]
