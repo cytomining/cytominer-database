5369033dc5d2b5a350f6489aeee871dedd394a7e
diskontinuum <frances.hubis@gmail.com>
2020-01-16 17:44:27 -0500

    Started integration of schema-checking

--- cytominer_database/ingest.py
+++ cytominer_database/ingest.py
@@ -35,6 +35,29 @@ Example::
     import cytominer_database.ingest
 
     cytominer_database.ingest.seed(source, target, config)
+
+"""
+
+
+# ToDo:
+"""
+- Build new schema
+Load a collection of tables and compare (1) number of columns and (2) type of every column.
+Build the correct schema:
+(1) Among all tables: Names of widest table (no missing columns). 
+(2) For each row, among all tables: Higher Type 
+   OR : simply convert all column types int -> float 
+
+- Get pyarrow table enforcing new schema
+# use new_pq_table = self.pa.Table.from_pandas(df, schema=pq_table.schema) 
+
+Example access: 
+old_field = f1.schema[faulty_indices[0]]
+old_name, old_type = old_field.name, old_field.type
+#check equivalence
+check_old_field = pa.field(old_name, old_type)
+old_field.equals(check_old_field) # True !!
+# --> can build field from field attributes!
 """
 
 import os
@@ -61,7 +84,27 @@ def __format__(name, header):
     return "{}_{}".format(name, header)
 
 
-def into(input, output, name, identifier,  writers, skip_table_prefix=False):
+def getDFfromTempDir(input, name, identifier, skip_table_prefix=False):
+    with backports.tempfile.TemporaryDirectory() as directory:
+        tmp_source = os.path.join(directory, os.path.basename(input))
+            # create a temporary CSV file which is identical to the input CSV file
+            # but with the column names prefixed with the name of the compartment
+            # (or `Image`, if this is an image CSV file, and `skip_table_prefix` is False)
+        with open(input, "r") as fin, open(tmp_source, "w") as fout:
+            reader = csv.reader(fin)
+            writer = csv.writer(fout)
+            headers = next(reader)
+            if not skip_table_prefix:
+                headers = [__format__(name, header) for header in headers]
+            # The first column is `TableNumber`, which is the unique identifier for the image CSV
+            headers = ["TableNumber"] + headers
+            writer.writerow(headers)
+            [writer.writerow([identifier] + row) for row in reader]
+            pandasDataFrame = pd.read_csv(source, index_col=0) 
+            return pandasDataFrame   
+
+
+def writeCSVtoDB(input, output, name, identifier,  writers, skip_table_prefix=False):
     """Ingest a CSV file into a table in a database.
 
     :param input: Input CSV file.
@@ -71,45 +114,40 @@ def into(input, output, name, identifier,  writers, skip_table_prefix=False):
     :param skip_table_prefix: True if the prefix of the table name should be excluded
      from the names of columns.
     """
+    # Create modified .csv in a temporary directory and get Pandas DataFrame
+    dataFrame = getModifiedDataframefromTempDir(input, name, identifier, skip_table_prefix=False)
 
-    with backports.tempfile.TemporaryDirectory() as directory:
-        # Question: Why do we need to reload the directory and generate the source?
-        # Why is "source" used as "fout"-writer later ?
-        # Isn't Source == input ? ("input" seems to have exactly the "directory + basename"-structure?)
-        source = os.path.join(directory, os.path.basename(input))
-
-        # Question: Can we move this section into the "SQLite" case?
-            # create a temporary CSV file which is identical to the input CSV file
-            # but with the column names prefixed with the name of the compartment
-            # (or `Image`, if this is an image CSV file, and `skip_table_prefix` is False)
-        with open(input, "r") as fin, open(source, "w") as fout:
-            reader = csv.reader(fin)
-            writer = csv.writer(fout)
-
-            headers = next(reader)
-            if not skip_table_prefix:
-                headers = [__format__(name, header) for header in headers]
-
-            # The first column is `TableNumber`, which is the unique identifier for the image CSV
-            headers = ["TableNumber"] + headers
-            writer.writerow(headers)
-            [writer.writerow([identifier] + row) for row in reader]
-
-        # The argument "writers" is an empty dict {} iff "SQLite" was selected as ingestion engine
-        if writers == {} :
-            # Now ingest the temp CSV file (with the modified column names) into the database backend
+    # If SQLite is the format specified in the config file, then the argument "writers" is an empty dict {}. 
+            # Then ingest the temp CSV file (with the modified column names) into the database backend
             # the rows of the CSV file are inserted into a table with name `name`.
-            with warnings.catch_warnings():
-                # Suppress the following warning on Python 3:
-                #
-                #   /usr/local/lib/python3.6/site-packages/odo/utils.py:128: DeprecationWarning: inspect.getargspec() is
-                #     deprecated, use inspect.signature() or inspect.getfullargspec()
-                warnings.simplefilter("ignore", category=DeprecationWarning)
-                engine = create_engine(output)
-                con = engine.connect()
-                df = pd.read_csv(source, index_col=0)
-                df.to_sql(name=name, con=con, if_exists="append")
-        else: # Write into open Parquet writers which are stored in the dictionary
+    if writers == {} :
+        with warnings.catch_warnings():
+        # Suppress the following warning on Python 3:
+        #
+        #   /usr/local/lib/python3.6/site-packages/odo/utils.py:128: DeprecationWarning: inspect.getargspec() is
+        #     deprecated, use inspect.signature() or inspect.getfullargspec()
+            warnings.simplefilter("ignore", category=DeprecationWarning)
+            engine = create_engine(output)
+            con = engine.connect()
+            df.to_sql(name=name, con=con, if_exists="append")
+    else: # Write into open Parquet writers which are stored in the dictionary
+      
+
+
+        # dataframe schema
+
+
+        # compare to reference schema
+
+        # type conversion if necessary
+
+        # add empty columns if necessary
+
+
+
+
+
+
             table           = pyarrow.csv.read_csv(source)
             parquetWriter   = writers[name]  # will return the opened writer of the image or compartment
             parquetWriter.write_table(table)
@@ -134,26 +172,67 @@ def checksum(pathname, buffer_size=65536):
 
     return result & 0xffffffff
 
-def getWriters(source, target,  writer_dict, tablePaths):
+def openWriters(source, target,  writer_dict, identifier, refTablePaths):
     # Comment:  Here we use a dictionary for four different writers,
     #           ..where key = compartment and  value = opened writer.
     #           Alternatively, we can dynamically allocate the writer
     #           ..variable with:  globals()["writer_" + name ] = ...
 
+    for refTablePath in refTablePaths:  # tablePaths = [image, compartments]
+        name, _   = os.path.splitext(os.path.basename(refTablePath)) # why do we need splitext?
+        name      = name.capitalize()
+"""
+At this point it would be easier to just get and pass the pyarrow schema. 
+However, later we load the .csv files to pandas data frames and want to be able
+to compare the "schemata" based on pandas data frames: If they disagree, we type-cast 
+the relevant columns. Only then do we convert the data frames to pyarrow. 
+Hence it is important to build a pandas dataframe for reference.
+"""
+    """
+
+    - We need to find the *right* schema before opening the writer!
+    - for now: 4 example files that have been converted and checked for completeness
+    ;;; and use them to generate reference schema.
+    - we need to store the pandas df schemata for reference later AND open a writer with a pyarrow schema
+    - later: write code to find example files
+
+    - Alternative : Write parquet files on an individual basis and deal with
+    ... the schema mess later (merging parquet schemata)
+
+
+
+    """ 
+    # Get reference schema. 
+    # During the loading and writing process, every table that has a different schema will be converted to the reference schema.
+    # (1) Padding: Missing columns will be added with null-values
+    # (2) Type casting: If a column type differs the column will be type-converted to the reference type.
+    # Hence it is important that the reference tables from "refTablePaths" contain all columns and the columns are of the right type.
+    # In our case this only means that integer types (values '0') are converted to float. 
+    # I.e. we could just check all columns for ints and convert them to float.
+            refPdDF                 = getDFfromTempDir(input=source, name=name, identifier, skip_table_prefix)
+            refPdSchema             = 
+
+            refPyTable              = pdDF.fromPandas()  #check exact function!
+            basename                = name + ".parquet"
+            # here: unpartitioned output directore
+            destination         = os.path.join(target, basename)
+            writer              = pq.ParquetWriter(destination, pyTable.schema)
+
+            writer_dict[name]   = [writer, refPdSchema, refPySchema]
+
+    return writer_dict
+
+def closeWriters(source, target,  writer_dict, identifier, tablePaths):
     for tablePath in tablePaths:  # tablePaths = [image, compartments]
         name, _   = os.path.splitext(os.path.basename(tablePath)) # why do we need splitext?
         name      = name.capitalize()
+        writer = writer_dict[name]
+        writer.close()
 
-        if name in writer_dict :    # close writer
-            writer = writer_dict[name]
-            writer.close()
-        else:                       # open writer
-            table               = pyarrow.csv.read_csv(tablePath)
-            basename            = name + ".parquet"
-            destination         = os.path.join(target, basename)
-            writer_dict[name]   = pq.ParquetWriter(destination, table.schema)
+def getrefSchema(directories, numberOfComparisons):
+    # Takes the first n = numberOfComparisons+1 tables and finds the "common" schema. 
 
-    return writer_dict
+
 
 
 def seed(source, target, config_file, skip_image_prefix=True):
@@ -192,12 +271,13 @@ def seed(source, target, config_file, skip_image_prefix=True):
             writer_dict = {}
             if engine == 'Parquet' :
                 tablePaths  = [image] + compartments
-                writer_dict = getWriters(source, target, writer_dict, tablePaths)
+                writer_dict = openWriters(source, target, writer_dict, tablePaths)
 
         # start ingestion
         # 1. ingest the image CSV
         name, _ = os.path.splitext(config_file["filenames"]["image"])
         name      = name.capitalize()
+        # one step :  name = os.path.splitext(config_file["filenames"]["image"])[0].capitalize
             # Claim:
             # os.path.splitext(config_file["filenames"]["image"]) == os.path.splitext(os.path.basename(image))
 
