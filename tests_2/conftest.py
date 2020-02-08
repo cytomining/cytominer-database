0e84e6838184cdeb2b82b7f9608b16dc524706b5
diskontinuum <frances.hubis@gmail.com>
2020-02-08 17:42:56 -0500

    Does not contain changes from colab notebook.

---
+++ tests_2/conftest.py
@@ -0,0 +1,134 @@
+import os
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--dataset", action="store", help="dataset to test")
+
+
+def pytest_generate_tests(metafunc):
+    if "dataset" in metafunc.fixturenames:
+        if metafunc.config.option.dataset is None:
+            metafunc.parametrize("dataset", ["htqc", "cellpainting", "qc"], indirect=True)
+        else:
+            assert metafunc.config.option.dataset in ["htqc", "cellpainting", "qc"]
+
+            metafunc.parametrize("dataset", [metafunc.config.option.dataset], indirect=True)
+
+
+@pytest.fixture
+def cellpainting():
+    """
+    Return configuration for a Cell Painting dataset.
+    - 3 compartments CSVs: Cells, Cytoplasm, Nuclei
+    - 1 image CSV
+    - No object.csv and therefore no munging
+    """
+    return {
+        "config": "config.ini",
+        "data_dir": "tests/data_b",
+        "image_csv": "Image.csv",
+        "ingest": [
+            {
+                "ncols": 586,
+                "nrows": 40,
+                "table": "Cells"
+            },
+            {
+                "ncols": 572,
+                "nrows": 40,
+                "table": "Cytoplasm"
+            },
+            {
+                "ncols": 6,
+                "nrows": 4,
+                "table": "image"
+            },
+            {
+                "ncols": 595,
+                "nrows": 40,
+                "table": "Nuclei"
+            }
+        ],
+        "munge": False
+    }
+
+@pytest.fixture
+def htqc():
+    """
+    Return configuration for a 3-channel image-based profiling dataset.
+    - 1 object CSV that comprises Cells, Cytoplasm, Nuclei
+    - 1 image CSV
+    - munging required
+    """
+    return {
+        "config": "config.ini",
+        "data_dir": "tests/data_a",
+        "munged_dir": "tests/data_a_munged",
+        "image_csv": "image.csv",
+        "ingest": [
+            {
+                "ncols": 294,
+                "nrows": 40,
+                "table": "Cells"
+            },
+            {
+                "ncols": 279,
+                "nrows": 40,
+                "table": "Cytoplasm"
+            },
+            {
+                "ncols": 229,
+                "nrows": 8,
+                "table": "image"
+            },
+            {
+                "ncols": 287,
+                "nrows": 40,
+                "table": "Nuclei"
+            }
+        ],
+        "munge": True
+    }
+
+
+@pytest.fixture
+def qc():
+    """
+    Return configuration for a QC dataset (only image table, no objects).
+    - 1 image CSV
+    - No object.csv and therefore no munging
+    """
+    return {
+        "config": None,
+        "data_dir": "tests/data_c",
+        "image_csv": "Image.csv",
+        "ingest": [
+            {
+                "nrows": 8,
+                "ncols": 229,
+                "table": "Image"
+            }
+        ],
+        "munge": True
+    }
+
+
+@pytest.fixture
+def dataset(request):
+    # Note that calling fixtures directly is deprecated:
+    # https://docs.pytest.org/en/latest/deprecations.html#calling-fixtures-directly
+    # Instead, use the request fixture to dynamically run the named fixture function:
+    # https://docs.pytest.org/en/latest/reference.html#_pytest.fixtures.FixtureRequest.getfixturevalue
+    dataset_param = request.param
+
+    if dataset_param == "htqc":
+        return request.getfixturevalue('htqc')
+
+    if dataset_param == "cellpainting":
+        return request.getfixturevalue('cellpainting')
+
+    if dataset_param == "qc":
+        return request.getfixturevalue('qc')
+
+    raise ValueError("No such dataset: {}".format(request.param))
---
+++ tests_2/test_munge.py
@@ -0,0 +1,32 @@
+import os
+
+import backports.tempfile
+import cytominer_database.munge
+import pandas as pd
+import pandas.util.testing
+
+
+def test_munge(dataset):
+    if not dataset["munge"]:
+        return
+
+    config_file = os.path.join(dataset["data_dir"], "config.ini")
+
+    with backports.tempfile.TemporaryDirectory() as temp_dir:
+        valid_directories = cytominer_database.munge.munge(
+            config_file=config_file,
+            source=dataset["data_dir"],
+            target=temp_dir
+        )
+
+        for directory in valid_directories:
+            for csv_filename in ["Cells.csv",  "Cytoplasm.csv", "Nuclei.csv"]:
+                input_csv = pd.read_csv(
+                    os.path.join(directory.replace(dataset["data_dir"], temp_dir), csv_filename)
+                )
+
+                output_csv = pd.read_csv(
+                    os.path.join(directory.replace(dataset["data_dir"], dataset["munged_dir"]), csv_filename)
+                )
+
+                pandas.util.testing.assert_frame_equal(input_csv, output_csv)
