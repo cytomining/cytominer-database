7f2604147c1cdf1fa03d20636be6f481f80631c8
diskontinuum <27351364+diskontinuum@users.noreply.github.com>
2020-02-24 10:43:44 +0100

    comma

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
         'click>=6.7',
         'configparser>=3.5.0',
         'csvkit>=1.0.2',
-        'pandas>=0.20.3',
+        'pandas>=0.20.3'
     ],
     license='BSD',
     url='https://github.com/cytomining/cytominer-database'
