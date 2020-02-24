a5af51cb8913fff46bf1607fe4419ba11d663581
diskontinuum <27351364+diskontinuum@users.noreply.github.com>
2020-02-24 10:42:42 +0100

    removed pyarrow from install_requires in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setuptools.setup(
         'configparser>=3.5.0',
         'csvkit>=1.0.2',
         'pandas>=0.20.3',
-        'pyarrow>=0.16.1'
     ],
     license='BSD',
     url='https://github.com/cytomining/cytominer-database'
