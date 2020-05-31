1bacfac39619e5621b0597d335096fb2590fc8d6
diskontinuum <27351364+diskontinuum@users.noreply.github.com>
2020-05-31 07:44:50 +0200

    removed docstring prose

--- cytominer_database/ingest_incl_parquet.py
+++ cytominer_database/ingest_incl_parquet.py
@@ -1,11 +1,3 @@
-# May 4th, TODO:
-# Main: Clean up (remaiming: tableSchema.py). Build base class and engine-extensions.
-# Other: - get rid of superfluous arguments. 
-# ---> skip_table/image_prefix
-    # alternative : instead of passing "skip_image_prefix", we could load the setting from the config file. Needs adjustments of tests.
-    # Currently: prefixes are never skipped for compartments.
-    # They are skipped for images by default, but can be added if skip_image_prefix is passed as True.
-# ----> write a new validate_csv_set() and deal with weird image/compartment output structure         
 """
 A mechanism to ingest CSV files into a database.
 
@@ -48,69 +40,9 @@ Example::
     source = path/to/plate_a
     target = path/to/output/directory
     config_path = path/to/plate_a/ingest_config.ini
-Notes: 
-* seed() will run regardless of where the config-file was stored, as long as its' full path is passed. 
-This is not the case for the command-line command, where the config-file is specified only by the basename 
-(e.g. ingest_config.ini) and is assumed to reside in path/to/plate_a.
+Example: 
+ 'file_1.csv', ..., 'file_n.csv' could be equivalent to 'Cells.csv', 'Cytoplasm.csv', 'Nuclei.csv', 'Image.csv', 'Object.csv'.
 
-* 'file_1.csv', ..., 'file_n.csv' is typically equivalent to 'Cells.csv', 'Cytoplasm.csv', 'Nuclei.csv', 'Image.csv', 'Object.csv'.
-
-"""
-
-"""
-* Extended explanations of how the PARQUET writer is used (see also the readme.rst).
-[Big Picure]
-When opening a Parquet file with the Pyarrow.parquet.writer, we use a reference table to set
-one fixed table schema (pyarrow.Table.schema) (per table kind).
-[Schema compatibility: column names]
-For each new table to be written to the open Parquet writer, schema compatibility is assured by aligning the dataframes. 
-The alignment assures that the new table will hold all columns present in the reference table, however, columns not present in the 
-reference table will be dropped from the new table. 
-Hence it is important to assure that the reference table has all columns that will be present in any of the tables of its kind. 
-
-[Schema compatibility: column types]
-Furthermore, the column types of columns with identical column names must agree. This has proven to be problematic
-when software further up the processing pipeline casts the types of its' output automatically. 
-The easiest solution for this is to convert all values in the .csv table to strings, however, this can be a disadvantage in the 
-processing tasks futher down the pipeline. The type incompatipility occurs infrequently though,
- e.g. when a 0-valued column is cast as 'int', instead of 'float', which makes the table schema incompatible with
-  the table schema from files with non-zero values in the same column with a 'float'-type). 
- Hence it is in our experience sufficient to convert integers to float-types. 
- The type conversion is applied explicitly and directly to both the reference table before opening the writer,
-  and to any table before appending it to the open writer. 
- The conversion type is set in the config file in the schema section, with key type_conversion.
-  Possible values are 'int2float' and 'all2string'. We do not use any automatic type-hierarchy-based casting
- (although this code was developed in the preliminary colaboratory notebook). 
- Note that schema incompatibilities are currently only solved for the Parquet option,
-  and are not resolved when writing the files to SQL with the SQLite option. 
-
-[Choosing the reference tables]
-The reference table is set by either
-(a) The widest table among a subset of tables sampled at random 
-(from all subdirectories of the source folder, e.g. "plate_a/" -> .csv files in set_1, ..., set_m ). 
-The sample size is determined as a fraction of the number of all available files of that kind,
-the value of which is a number in [0,1] and is set in the config file with the key "ref_fraction".
-The tables are sampled iid within one table kind and independent of other table kinds. 
-or
-(b) The reference tables are stored in a prespecified folder located at "reference_option = path/to/reference/folder", 
-as specified in the config file (path is relative to source_directory).
-
-[Reference dictionary keys]
-To avoid errors from inconsistent capitalization of .csv file names, the keys of the reference dictionaries
-(referencing the writer and the fixed schema) are built automatically from the basenames of existing CSV files
-in specified directories. This means that per default, object.csv is also read and written.
-However, this can be surpressed by explicitly excluding them in write.csv_to_sqlite() and write.csv_to_parquet().
- 
-* Special cases
-[DONE]
-- missing columns
-- type disagreements the same column headers of different sets (same table type, e.g. Cells.csv)
-- null values etc in tables that are read in after the correct reference table has been determined
-
-[TODO]
-- build tests that compare the table content, not just size
-- build tests for these special cases
-- what happens if by accident the reference table has the wrong type?
 """
 
 import os
