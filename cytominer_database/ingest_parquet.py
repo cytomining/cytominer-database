d1610fcedef52d6f89b74a102dd045dd99644a39
diskontinuum <27351364+diskontinuum@users.noreply.github.com>
2020-07-24 17:38:06 +0200

    renamed module ingest_incl_parquet.py to ingest_parquet.py . Changed imports and tests accordingly.

---
+++ cytominer_database/ingest_parquet.py
@@ -0,0 +1,142 @@
+"""
+A mechanism to ingest CSV files into a database.
+
+In morphological profiling experiments, a CellProfiler pipeline is often run in parallel across multiple images and
+produces a set of CSV files. For example, imaging a 384-well plate, with 9 sites per well, produces 384 * 9 images;
+a CellProfiler process may be run on each image, resulting in a 384*9 output directories (each directory typically
+contains one CSV file per compartment (e.g. Cells.csv, Cytoplasm.csv, Nuclei.csv) and one CSV file for per-image
+measurements (e.g. Image.csv).
+
+``cytominer_database.ingest.seed`` can be used to read all these CSV files into a database backend. SQLite is the
+recommended engine, but ingest will likely also work with PostgreSQL and MySQL.
+
+``cytominer_database.ingest.seed`` assumes a directory structure like shown below:
+
+| plate_a/
+|   set_1/
+|       file_1.csv
+|       file_2.csv
+|       ...
+|       file_n.csv
+|   set_2/
+|       file_1.csv
+|       file_2.csv
+|       ...
+|       file_n.csv
+|   ...
+|   set_m/
+|       file_1.csv
+|       file_2.csv
+|       ...
+|       file_n.csv
+
+Example::
+
+    import cytominer_database.ingest
+
+    cytominer_database.ingest.seed(source, target, config_path)
+
+    where
+    source = path/to/plate_a
+    target = path/to/output/directory
+    config_path = path/to/plate_a/ingest_config.ini
+Example: 
+ 'file_1.csv', ..., 'file_n.csv' could be equivalent to 'Cells.csv', 'Cytoplasm.csv', 'Nuclei.csv', 'Image.csv', 'Object.csv'.
+
+"""
+import csv
+import click
+import zlib
+import pandas as pd
+import backports.tempfile
+import sqlalchemy.exc
+from sqlalchemy import create_engine
+import pyarrow
+import pyarrow.parquet as pq
+import pyarrow.csv
+import collections
+import numpy as np
+import cytominer_database
+import cytominer_database.utils
+import cytominer_database.write
+import cytominer_database.tableSchema
+
+
+def seed(source, output_path, config_path, skip_image_prefix=True, directories=None):
+    """
+    Main function. Loads configuration. Opens ParquetWriter.
+    Calls writer function. Closes ParquetWriter.
+    :source: path to parent directory containing subdirectories, e.g. "path/plate_a/"
+    :output_path: path of destination folder
+    :config_path: full path of configuration file, e.g. path/to/plate_a/ingest_config.ini
+    :skip_image_prefix: Boolean value specifying if the column headers of
+     the image.csv files should be prefixed with the table name ("Image").
+    :directories: Pass subdirectories path list instead of generating a
+        path list from the source path. Added to test special cases. Can be removed.
+
+    """
+    config = cytominer_database.utils.read_config(config_path)
+    engine = config["ingestion_engine"]["engine"]
+
+    # get dictionary that contains [name]["writer"], [name]["schema"], [name]["pandas_dataframe"]
+    writers_dict = cytominer_database.tableSchema.open_writers(
+        source, output_path, config, skip_image_prefix
+    )
+    # lists the subdirectories that contain CSV files
+    if not directories:
+        directories = sorted(list(cytominer_database.utils.find_directories(source)))
+    # ----------------------------- iterate over subfolders in source folder------------------------------------
+    for directory in directories:
+        # ....................... get input .csv file paths ......................
+        try:
+            compartments, image = cytominer_database.utils.validate_csv_set(
+                config, directory
+            )
+        except IOError as e:
+            click.echo(e)
+            continue
+
+        identifier = checksum(image)
+        csv_locations = [image] + compartments
+        # ----------------------------------- iterate over .csv's ---------------------------------------
+        for input_path in csv_locations:
+            table_name = cytominer_database.utils.get_name(input_path)
+            dataframe = cytominer_database.load.get_and_modify_df(
+                input_path, identifier, skip_image_prefix
+            )
+            cytominer_database.utils.type_convert_dataframe(dataframe, config)
+            cytominer_database.write.write_to_disk(
+                dataframe, table_name, output_path, engine, writers_dict
+            )
+    # --------------------------------------- close writers ---------------------------------------------
+    close_writers(writers_dict, engine)
+
+
+# --------------------------------------------- end ---------------------------------------------------
+
+
+def close_writers(writers_dict, engine):
+    """
+    Close the Parquet writers
+    :param writers_dict: dictionary containing the references to the writers of every table kind
+    :param engine: "Parquet" or "SQLite
+    """
+    if engine == "Parquet":
+        for name in writers_dict.keys():
+            writers_dict[name]["writer"].close()
+
+
+def checksum(pathname, buffer_size=65536):
+    """
+    Generate a 32-bit unique identifier for a file.
+    :param pathname: input file
+    :param buffer_size: buffer size
+    """
+    with open(pathname, "rb") as stream:
+        result = zlib.crc32(bytes(0))
+        while True:
+            buffer = stream.read(buffer_size)
+            if not buffer:
+                break
+            result = zlib.crc32(buffer, result)
+    return result & 0xFFFFFFFF
---
+++ tests/test_ingest_parquet.py
@@ -0,0 +1,71 @@
+import os
+
+import pandas as pd
+import backports.tempfile
+from sqlalchemy import create_engine
+
+# import cytominer_database.ingest
+import cytominer_database.ingest_parquet
+import cytominer_database.munge
+import pytest
+
+
+@pytest.mark.parametrize("config_choice", ["config_Parquet.ini", "config_SQLite.ini"])
+def test_seed(dataset, config_choice):
+    data_dir = dataset["data_dir"]
+    munge = dataset["munge"]
+    ingest = dataset["ingest"]
+    config_path = os.path.join(data_dir, config_choice)
+    # Get database engine option
+    # (for this we need to read the config file from the path here already!)
+    config_file = cytominer_database.utils.read_config(config_path)
+    engine_type = config_file["ingestion_engine"]["engine"]
+
+    if munge:
+        cytominer_database.munge.munge(config_path, data_dir)
+
+    with backports.tempfile.TemporaryDirectory() as temp_dir:
+        # set up
+        if engine_type == "Parquet":
+            # create output directory
+            target = os.path.join(temp_dir, "test_parquet_output")
+            try:
+                os.stat(target)
+            except:
+                os.mkdir(target)
+
+        elif engine_type == "SQLite":
+            sqlite_file = os.path.join(temp_dir, "test.db")
+            target = "sqlite:///{}".format(str(sqlite_file))
+
+        # run program
+        cytominer_database.ingest_parquet.seed(
+            config_path=config_path, source=data_dir, output_path=target
+        )
+        # necessary ?
+        if engine_type == "Parquet":
+            assert os.path.exists(str(target))
+        elif engine_type == "SQLite":
+            assert os.path.exists(str(sqlite_file))
+
+        for blob in ingest:
+            table_name = blob["table"].capitalize()
+
+            if engine_type == "Parquet":
+                # df = pd.read_parquet(path=target, engine ='pyarrow') # ignore column 0 (columns=[1:])? Column 0 should be read only as an index (index_col=0) ?
+                basename = ".".join([table_name, "parquet"])
+                compartment_path = os.path.join(target, basename)
+                df = pd.read_parquet(path=compartment_path)
+
+            elif engine_type == "SQLite":
+                engine = create_engine(target)
+                con = engine.connect()
+                df = pd.read_sql(sql=table_name, con=con)
+            assert df.shape[0] == blob["nrows"]
+            assert df.shape[1] == blob["ncols"] + 1
+
+            if table_name.lower() != "image":
+                assert (
+                    df.groupby(["TableNumber", "ImageNumber"]).size().sum()
+                    == blob["nrows"]
+                )
