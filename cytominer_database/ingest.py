2d2e657c290f3d6a3bd24ed1a9e2ca1699c1cea2
Beth Cimini <bethac07@users.noreply.github.com>
2021-06-22 19:41:48 -0400

    Move database connection from `into` to `seed`, close connection

--- cytominer_database/ingest.py
+++ cytominer_database/ingest.py
@@ -47,6 +47,7 @@ import pandas as pd
 import backports.tempfile
 import sqlalchemy.exc
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 import cytominer_database.utils
 
@@ -58,7 +59,7 @@ def __format__(name, header):
     return "{}_{}".format(name, header)
 
 
-def into(input, output, name, identifier, skip_table_prefix=False):
+def into(input, output, name, identifier, con, skip_table_prefix=False):
     """Ingest a CSV file into a table in a database.
 
     :param input: Input CSV file.
@@ -99,9 +100,6 @@ def into(input, output, name, identifier, skip_table_prefix=False):
             #     deprecated, use inspect.signature() or inspect.getfullargspec()
             warnings.simplefilter("ignore", category=DeprecationWarning)
 
-            engine = create_engine(output)
-            con = engine.connect()
-
             df = pd.read_csv(source, index_col=0)
             df.to_sql(name=name, con=con, if_exists="append")
 
@@ -140,6 +138,9 @@ def seed(source, target, config_file, skip_image_prefix=True):
     # list the subdirectories that contain CSV files
     directories = sorted(list(cytominer_database.utils.find_directories(source)))
 
+    engine = create_engine(target, poolclass=NullPool)
+    con = engine.connect()
+    
     for directory in directories:
 
         # get the image CSV and the CSVs for each of the compartments
@@ -159,7 +160,7 @@ def seed(source, target, config_file, skip_image_prefix=True):
 
         # ingest the image CSV
         try:
-            into(input=image, output=target, name=name.capitalize(), identifier=identifier,
+            into(input=image, output=target, name=name.capitalize(), identifier=identifier, con=con,
                  skip_table_prefix=skip_image_prefix)
         except sqlalchemy.exc.DatabaseError as e:
             click.echo(e)
@@ -170,4 +171,5 @@ def seed(source, target, config_file, skip_image_prefix=True):
         for compartment in compartments:
             name, _ = os.path.splitext(os.path.basename(compartment))
 
-            into(input=compartment, output=target, name=name.capitalize(), identifier=identifier)
+            into(input=compartment, output=target, name=name.capitalize(), identifier=identifier, con=con)
+    con.close()
